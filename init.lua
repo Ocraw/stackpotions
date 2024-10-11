6d2b80ffb319ed2d6b4a2603a528136f635b7688
Ocraw <lamaildelmarco@gmail.com>
2024-10-11 23:05:30 +0200

    Update

--- init.lua
+++ init.lua
@@ -3,14 +3,14 @@
 stackpotions = {}
 stackpotions.stack_n = minetest.settings:get("stackpotions_count") or 9
 stackpotions.stands_node_names = {
-  "mcl_brewing:stand_000",
-  "mcl_brewing:stand_100",
-  "mcl_brewing:stand_010",
-  "mcl_brewing:stand_001",
-  "mcl_brewing:stand_110",
-  "mcl_brewing:stand_101",
-  "mcl_brewing:stand_011",
-  "mcl_brewing:stand_111",
+	"mcl_brewing:stand_000",
+	"mcl_brewing:stand_100",
+	"mcl_brewing:stand_010",
+	"mcl_brewing:stand_001",
+	"mcl_brewing:stand_110",
+	"mcl_brewing:stand_101",
+	"mcl_brewing:stand_011",
+	"mcl_brewing:stand_111",
 }
 
 local function brewable(inv)
@@ -64,91 +64,6 @@ local function take_fuel (pos, meta, inv)
 	end
 end
 
-local function brewing_stand_timer(pos, elapsed)
-	-- Inizialize metadata
-	local meta = minetest.get_meta(pos)
-
-	-- Number of seconds of fuel remaining to be consumed.
-	local fuel_timer = meta:get_float ("fuel_timer_new")
-	local stand_timer = meta:get_float ("stand_timer")
-	local inv = meta:get_inventory ()
-	local brew_output, d
-	local input_count, formspec, fuel_percent, brew_percent
-
-	brew_output = brewable(inv)
-	if brew_output then
-		if fuel_timer <= 0 then -- Is more fuel required?
-		fuel_timer = take_fuel (pos, meta, inv)
-		end
-
-		-- If enough fuel remains, continue.
-		if fuel_timer > 0 then
-		fuel_timer = fuel_timer - elapsed
-		stand_timer = stand_timer + elapsed
-		d = 0.5
-		minetest.add_particlespawner({
-			amount = 4,
-			time = 1,
-			minpos = {x=pos.x-d, y=pos.y+0.5, z=pos.z-d},
-			maxpos = {x=pos.x+d, y=pos.y+2, z=pos.z+d},
-			minvel = {x=-0.1, y=0, z=-0.1},
-			maxvel = {x=0.1, y=0.5, z=0.1},
-			minacc = {x=-0.05, y=0, z=-0.05},
-			maxacc = {x=0.05, y=.1, z=0.05},
-			minexptime = 1,
-			maxexptime = 2,
-			minsize = 0.5,
-			maxsize = 2,
-			collisiondetection = true,
-			vertical = false,
-			texture = "mcl_brewing_bubble_sprite.png",
-		})
-
-		-- Replace the stand item with the brew result
-		if stand_timer >= BREW_TIME then
-			input_count = inv:get_stack("input",1):get_count()
-			if (input_count-1) ~= 0 then
-			local stack
-				= inv:get_stack("input",1):get_name().." "..(input_count-1)
-			inv:set_stack("input", 1, stack)
-			else
-			inv:set_stack("input", 1, "")
-			end
-
-			for i=1, inv:get_size("stand") do
-			if brew_output[i] then
-				minetest.sound_play("mcl_brewing_complete",
-						{pos=pos, gain=0.4, max_hear_range=6}, true)
-				inv:set_stack ("stand", i, brew_output[i])
-				minetest.sound_play("mcl_potions_bottle_pour",
-						{pos=pos, gain=0.6, max_hear_range=6}, true)
-			end
-			end
-			stand_timer = 0
-		end
-		end
-	end
-
-	-- The formspec must be updated after each change.
-	fuel_percent = 100 - math.floor (math.max (fuel_timer, 0)
-					 / BURN_TIME * 100)
-	brew_percent = math.floor(stand_timer/BREW_TIME*100)
-	formspec = active_brewing_formspec(fuel_percent, brew_percent*1 % 100)
-
-	local value = true
-	-- If the stand becomes inactive, as when no fuel remains or
-	-- no valid recipe exists, cancel the timer.
-	if fuel_timer <= 0 or not brew_output then
-		minetest.get_node_timer (pos):stop ()
-		value = false
-	end
-
-	meta:set_float("fuel_timer_new", fuel_timer)
-	meta:set_float("stand_timer", stand_timer)
-	meta:set_string("formspec", formspec)
-	return value
-end
-
 local function sort_stack(stack)
 	if stack:get_name() == "mcl_mobitems:blaze_powder" then
 		return "fuel"
@@ -174,9 +89,8 @@ function stackpotions.allow_put(pos, listname, stack_id, stack, player)
 	local trg = sort_stack(stack)
 	local trg_stack = inv:get_stack("stand", stack_id)
 	if listname == "stand" then
-	  -- If any item is already in stand list[id], no allowed put there
-		--if trg ~= "stand" or ((trg_stack:get_count() > 0) and (trg_stack:get_meta() == stack:get_meta())) then
-		if trg ~= "stand" or (trg_stack:get_count() > 0) then
+		-- If any item is already in stand list[id], no allowed put there
+		if trg ~= "stand" or (not trg_stack:is_empty()) then
 			return 0
 		end
 		return 1
@@ -184,20 +98,19 @@ function stackpotions.allow_put(pos, listname, stack_id, stack, player)
 		if trg ~= "fuel" then return 0 end
 	elseif listname == "sorter" then
 		if trg then
-		    if trg == "stand" then
-		      local r = 0
-		      -- Iterate stand list to get the number of allowed potions
-		      -- based on how many are already in
-		      for i=1,inv:get_size("stand") do
-		        local trg_stack = inv:get_stack("stand", i)
-		        --if not ((trg_stack:get_count() > 0) and (trg_stack:get_meta() == stack:get_meta())) then
-		        if not (trg_stack:get_count() > 0) then
-		          r = r + 1
-		        end
-		      end
-		      -- Returning calculated allowed
-		      return r
-		    end
+			if trg == "stand" then
+				local r = 0
+				-- Iterate stand list to get the number of allowed potions
+				-- based on how many are already in
+				for i=1,inv:get_size("stand") do
+					local trg_stack = inv:get_stack("stand", i)
+					if trg_stack:is_empty() then
+						r = r + 1
+					end
+				end
+				-- Returning calculated allowed
+				return r
+			end
 
 			local stack1 = ItemStack(stack):take_item()
 			if inv:room_for_item(trg, stack) then
@@ -211,7 +124,7 @@ function stackpotions.allow_put(pos, listname, stack_id, stack, player)
 	return stack:get_count()
 end
 
-local function start_stand_if_not_empty(pos)
+function stackpotions.start_stand_if_not_empty(pos)
 	local meta = minetest.get_meta(pos)
 	local inv = meta:get_inventory()
 	local str = ""
@@ -223,6 +136,10 @@ local function start_stand_if_not_empty(pos)
 		end
 	end
 	minetest.swap_node(pos, {name = "mcl_brewing:stand_"..str})
+	if not brewable(inv) then
+		minetest.get_node_timer (pos):stop ()
+		return
+	end
 	minetest.get_node_timer(pos):start(1.0)
 end
 
@@ -230,15 +147,14 @@ function stackpotions.on_put(pos, listname, stack_id, stack, player)
 	local meta = minetest.get_meta (pos)
 	local inv = meta:get_inventory ()
 	local s_flag_in
-
 	if listname == "sorter" then
 		listname = sort_stack (stack)
 		if not (listname == "stand") then
-		  inv:add_item(listname, stack)
+			inv:add_item(listname, stack)
 		else
-		  -- Set the number of potions just inserted in the sorter
-		  s_flag_in = inv:get_stack("sorter", 1):get_count()
-	    end
+			-- Set the number of potions just inserted in the sorter
+			s_flag_in = inv:get_stack("sorter", 1):get_count()
+		end
 		inv:set_stack("sorter", 1, ItemStack(""))
 	end
 
@@ -252,26 +168,26 @@ function stackpotions.on_put(pos, listname, stack_id, stack, player)
 	end
 
 	if listname == "stand" then
-	  local new_stack = ItemStack(stack)
-	  new_stack:set_count(1)
-	  local r=0
-	  if s_flag_in then
-	    -- Iterate for how many potions were inserted
-	    for _=1, s_flag_in do
-	      -- Iterate stand slots and set potion only if not already in.
-	      for i=1, inv:get_size(listname) do
-	        local trg_stack = inv:get_stack(listname, i)
-	        --if not ((trg_stack:get_count() > 0) and (trg_stack:get_meta() == stack:get_meta())) then
-	        if not (trg_stack:get_count() > 0) then
-	  	      inv:set_stack(listname, i, new_stack)
-	  	      break
-	  	    end
-	  	  end
-	    end
-		start_stand_if_not_empty(pos)
-		return
-	  end
-    end
+		local new_stack = ItemStack(stack)
+		new_stack:set_count(1)
+		local r=0
+		-- If sorter has any potions
+		if s_flag_in then
+			-- Iterate for how many potions were inserted
+			for _=1, s_flag_in do
+				-- Iterate stand slots and set potion only if not already in.
+				for i=1, inv:get_size(listname) do
+					local trg_stack = inv:get_stack(listname, i)
+					if trg_stack:is_empty() then
+						inv:set_stack(listname, i, new_stack)
+						break
+					end
+				end
+			end
+			stackpotions.start_stand_if_not_empty(pos)
+			return
+		end
+	end
 	minetest.get_node_timer (pos):start (1.0)
 end
 
@@ -302,19 +218,20 @@ function stackpotions.hopper_in(pos, to_pos)
 				return sort_stack (itemstack) == "stand"
 			end)
 			if slot_id then
-			  local stack = sinv:get_stack("main", slot_id)
-			  local new_stack = ItemStack(stack)
-			  for i=1, dinv:get_size("stand") do
-			    -- Iterate and take/set only if not already in.
-			    if not (dinv:get_stack("stand", i):get_count() > 0) then
-			      new_stack:set_count(1)
-			      dinv:set_stack("stand", i, new_stack)
-			      stack:take_item()
-			      sinv:set_stack("main", slot_id, stack)
-				  start_stand_if_not_empty(to_pos)
-				  break
-			    end
-			  end
+				local stack = sinv:get_stack("main", slot_id)
+				local new_stack = ItemStack(stack)
+				for i=1, dinv:get_size("stand") do
+					-- Iterate and take/set only if not already in.
+					--if not (dinv:get_stack("stand", i):get_count() > 0) then
+					if dinv:get_stack("stand", i):is_empty() then
+						new_stack:set_count(1)
+						dinv:set_stack("stand", i, new_stack)
+						stack:take_item()
+						sinv:set_stack("main", slot_id, stack)
+						stackpotions.start_stand_if_not_empty(to_pos)
+						break
+					end
+				end
 			end
 		end
 		return true
@@ -337,32 +254,30 @@ function stackpotions.hopper_out(pos, to_pos)
 	if slot_id then
 		mcl_util.move_item(sinv, "stand", slot_id, dinv, "main")
 	end
-	start_stand_if_not_empty(pos)
+	stackpotions.start_stand_if_not_empty(pos)
 	return true
 end
 
 minetest.register_on_mods_loaded(function()
-  -- Override stands
-  for _,s in pairs(stackpotions.stands_node_names) do
-    minetest.override_item(s, {
-	allow_metadata_inventory_put = stackpotions.allow_put,
-	allow_metadata_inventory_move = stackpotions.allow_move,
-	on_metadata_inventory_put = stackpotions.on_put,
-	on_metadata_inventory_take = stackpotions.start_stand_if_not_empty,
-	_on_hopper_in = stackpotions.hopper_in,
-	_on_hopper_out = stackpotions.hopper_out,
-    })
-  end
-  -- Override potions
-  local i=0
-  for name,def in pairs(minetest.registered_items) do
-    if string.find(name, "mcl_potions:") then
-      if def.stack_max == 1 then
-        minetest.override_item(name, {stack_max = stackpotions.stack_n})
-        i=i+1
-      end
-    end
-  end
-  minetest.log("action", "Stackpotions: overridden "..i.." potions to stack in " ..stackpotions.stack_n)
+	-- Override stands
+	for _,stand_name in pairs(stackpotions.stands_node_names) do
+		minetest.override_item(stand_name, {
+		allow_metadata_inventory_put = stackpotions.allow_put,
+		allow_metadata_inventory_move = stackpotions.allow_move,
+		on_metadata_inventory_put = stackpotions.on_put,
+		on_metadata_inventory_take = stackpotions.start_stand_if_not_empty,
+		_on_hopper_in = stackpotions.hopper_in,
+		_on_hopper_out = stackpotions.hopper_out,
+		})
+	end
+	-- Override potions
+	local i=0
+	for name,def in pairs(minetest.registered_items) do
+		if string.find(name, "mcl_potions:") and def.stack_max == 1 then
+			minetest.override_item(name, {stack_max = stackpotions.stack_n})
+			i=i+1
+		end
+	end
+	minetest.log("action", "Stackpotions: overridden "..i.." potions to stack in " ..stackpotions.stack_n)
 end)
 
